a279bfec5720c9aad4f07d2df098024addf5a925
Abhishek Muvvala <abhishekmuvvala@Abhisheks-MacBook-Air.local>
2021-03-17 16:58:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CityInput from "./components/CityInput";
+import CityWeather from "./components/CityWeather";
+
+
+function App() {
+  const [city,setCity] = React.useState("");
+  const[cityWeather,setCityWeather] = React.useState({});
+
+  const fechCityWeather =()=> {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=678fd9c8e7649b6bc66a8f6e86ad22b2`
+    ).then((res)=>res.json())
+     .then((result)=>{
+        // console.log("result is result",result);
+        setCityWeather(result);
+     });
+  };
+  return (
+
+    <div className="App">
+      <CityInput city={city} setCity={setCity} fechCityWeather={fechCityWeather} />
+      {/* use city weather data to show it  on the screen */}
+      <CityWeather cityWeather={cityWeather}/>
+    </div>
+  );
+}
+
+export default App;
